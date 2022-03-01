a2520b36d5e54f07d1cf0aad24a087822d90c02b
Regina Chua <reginachua@Reginas-MacBook-Pro.local>
2022-03-01 17:09:48 +0800

    update entities

--- src/main/java/com/example/demo/Menu/Menu.java
+++ src/main/java/com/example/demo/Menu/Menu.java
@@ -1,6 +1,9 @@
 package com.example.demo.Menu;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+
+import com.example.demo.Business.Business;
+
 import lombok.*;
 
 @Entity
@@ -11,5 +14,17 @@ import lombok.*;
 @NoArgsConstructor
 @EqualsAndHashCode
 public class Menu {
-    
+    private  @Id @Column(name = "mid")@GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
+
+    @OneToOne
+    @JoinColumn(name="bid")
+    private Business business;
+
+    @NotNull(message = "Menu name should not be null")  // I think that menu do not need a name 
+    private String name;
+
+    private float price;
+
+
+
 }
--- src/main/java/com/example/demo/Order/Order.java
+++ src/main/java/com/example/demo/Order/Order.java
@@ -1,7 +1,12 @@
 package com.example.demo.Order;
 import javax.persistence.*;
 import javax.validation.constraints.*;
+
+import com.example.demo.Business.Business;
+import com.example.demo.Customer.Customer;
+
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -10,6 +15,27 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Table(name = "orders", schema = "cs206")
 public class Order {
+    private  @Id @Column(name = "oid") @GeneratedValue (strategy = GenerationType.IDENTITY) Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "bid")
+    private Business business;
+
+    @ManyToOne
+    @JoinColumn(name = "cid")
+    private Customer customer;
+
+    @NotNull(message = "Order name should not be null")  // I think that order do not need a name 
+    private String name;
+
+    @Column(name = "total_price")
+    private float price;
+
+    @NotNull(message = "date cannot be null")
+    @Column(name="odate")
+    private LocalDate date; 
+
     
 }
