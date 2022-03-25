3c7cb8540a2bd00b8ab513c0c17528259733f57a
Jasmine Quek <jasminequek.2020@sis.smu.edu.sg>
2022-03-25 12:50:37 +0800

    some changes

---
+++ src/main/java/com/example/demo/Business/test.java
@@ -0,0 +1,11 @@
+// package com.example.demo.Business;
+
+// public class test {
+//     private static BusinessServiceImpl businessService;
+//     public static void main(String[] args) {
+// 		// SpringApplication.run(DemoApplication.class, args);
+
+// 		Business biz = new Business("XYZ123", "DTF", "83335668", "www.dintaifung.sg");
+// 		businessService.addBusiness(biz);
+// 	}
+// }
--- src/main/java/com/example/demo/Mall/MallRepository.java
+++ src/main/java/com/example/demo/Mall/MallRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.Mall;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
+
+@Repository
 public interface MallRepository extends JpaRepository<Mall, Long>{
     
 }
